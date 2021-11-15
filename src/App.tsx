e5345d04332919455f6675fc5f37b22ab35babb5
Zechariah <zechariahtan144@gmail.com>
2021-11-15 13:25:20 +0800

    Added different screens and bottom nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { NavigationContainer } from "@react-navigation/native"
 import React from "react"
-import {
-	SafeAreaView,
-	StatusBar,
-	useColorScheme,
-} from "react-native"
+import Games from "./pages/Games"
+import Hanguls from "./pages/Hanguls"
+import Lessons from "./pages/Lessons"
+
+const Tab = createBottomTabNavigator()
 
 const App = (): JSX.Element => {
-	const isDarkMode = useColorScheme() === "dark"
-
 	return (
-		<SafeAreaView>
-			<StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-		</SafeAreaView>
+		<NavigationContainer>
+			<Tab.Navigator initialRouteName="Lessons" backBehavior="history">
+				<Tab.Screen name="Lessons" component={Lessons} />
+				<Tab.Screen name="Games" component={Games} />
+				<Tab.Screen name="Hanguls" component={Hanguls} />
+			</Tab.Navigator>
+		</NavigationContainer>
 	)
 }
 
