6303e02357fb5ea30f7d62de699cb7c2092e767a
Zechariah <zechariahtan144@gmail.com>
2021-11-15 13:41:11 +0800

    Included Magnus UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { NavigationContainer } from "@react-navigation/native"
 import React from "react"
+import { ThemeProvider } from "react-native-magnus"
 import Games from "./pages/Games"
 import Hanguls from "./pages/Hanguls"
 import Lessons from "./pages/Lessons"
@@ -9,13 +10,15 @@ const Tab = createBottomTabNavigator()
 
 const App = (): JSX.Element => {
 	return (
-		<NavigationContainer>
-			<Tab.Navigator initialRouteName="Lessons" backBehavior="history">
-				<Tab.Screen name="Lessons" component={Lessons} />
-				<Tab.Screen name="Games" component={Games} />
-				<Tab.Screen name="Hanguls" component={Hanguls} />
-			</Tab.Navigator>
-		</NavigationContainer>
+		<ThemeProvider>
+			<NavigationContainer>
+				<Tab.Navigator initialRouteName="Lessons" backBehavior="history">
+					<Tab.Screen name="Lessons" component={Lessons} />
+					<Tab.Screen name="Games" component={Games} />
+					<Tab.Screen name="Hanguls" component={Hanguls} />
+				</Tab.Navigator>
+			</NavigationContainer>
+		</ThemeProvider>
 	)
 }
 
