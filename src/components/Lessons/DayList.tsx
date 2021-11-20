6134a4a6e253b61db8afa659f5b7bff3b66f4d4a
Zechariah <zechariahtan144@gmail.com>
2021-11-21 00:49:54 +0800

    Visualize redux state

--- src/components/Lessons/DayList.tsx
+++ src/components/Lessons/DayList.tsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react"
+import React, { useCallback, useContext, useEffect } from "react"
 import { Button, Div, Icon, Text } from "react-native-magnus"
 import { Lessons as lessons } from "../../data.json"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native"
 import { SetTitleContext } from "../../App"
 import { useIsFocused } from "@react-navigation/native"
+import { useSelector } from "react-redux"
 
 type Props = NativeStackScreenProps<iLessonsStackParamList, "DayList">
 
@@ -14,6 +15,7 @@ const DayList = (props: Props): JSX.Element => {
 	//#region Hooks
 	const setTitle = useContext(SetTitleContext)
 	const isFocused = useIsFocused()
+	const progress = useSelector(state => state.lessons[month - 1])
 	//#endregion
 
 	//#region Effects
@@ -25,6 +27,19 @@ const DayList = (props: Props): JSX.Element => {
 	//#endregion
 
 	//#region Functions
+	const getBackgroundColor = useCallback(
+		(day: number) => {
+			if (progress[day - 1].completed) {
+				return "#c6f6d5"
+			}
+			if (Object.keys(progress[day - 1].qna).length > 0) {
+				return "#fefcbf"
+			}
+			return "#fed7d7"
+		},
+		[progress]
+	)
+
 	const handlePrevious = () => {
 		props.navigation.navigate("DayList", { month: month - 1 })
 	}
@@ -40,28 +55,37 @@ const DayList = (props: Props): JSX.Element => {
 
 	return (
 		<SafeAreaView>
-			<Div
-				justifyContent="space-between"
-				mt="lg"
-				mx="lg"
-				p="lg"
-				bg="white"
-				rounded="sm"
-				shadow="sm"
-				row>
-				<Button disabled={month === 1} bg="white" h={40} w={40} onPress={handlePrevious}>
-					<Icon name="left" fontSize={16} />
-				</Button>
-				<Text textAlign="center">Month {month}</Text>
-				<Button disabled={month === 5} bg="white" h={40} w={40} onPress={handleNext}>
-					<Icon name="right" fontSize={16} />
-				</Button>
-			</Div>
 			<ScrollView style={styles.scroll}>
+				<Div
+					justifyContent="space-between"
+					mb="lg"
+					mx="lg"
+					p="lg"
+					bg="white"
+					rounded="sm"
+					shadow="sm"
+					row>
+					<Button
+						disabled={month === 1}
+						bg="white"
+						h={40}
+						w={40}
+						onPress={handlePrevious}>
+						<Icon name="left" fontSize={16} />
+					</Button>
+					<Text textAlign="center">Month {month}</Text>
+					<Button disabled={month === 5} bg="white" h={40} w={40} onPress={handleNext}>
+						<Icon name="right" fontSize={16} />
+					</Button>
+				</Div>
 				{(lessons as iLesson[][])[month - 1].map((lesson, i) => (
 					<View key={i}>
 						<TouchableOpacity
-							style={styles.touchable}
+							style={[
+								styles.touchable,
+								{ backgroundColor: getBackgroundColor(i + 1) },
+								{ marginBottom: i === 29 ? 10 : 0 }
+							]}
 							onPress={() => handleLesson(lesson, i)}>
 							<Text>
 								Day {i + 1} - {lesson.title}
@@ -83,8 +107,7 @@ const styles = StyleSheet.create({
 		marginTop: 8,
 		marginStart: 10,
 		marginEnd: 10,
-		borderRadius: 10,
-		backgroundColor: "#fefefe"
+		borderRadius: 10
 	}
 })
 
