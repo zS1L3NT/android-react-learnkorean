1a9cbeb5a1bba85b3fb70b3843e88287d321c6f7
Zechariah <zechariahtan144@gmail.com>
2021-11-21 15:23:14 +0800

    Show month progress

--- src/components/Lessons/MonthList.tsx
+++ src/components/Lessons/MonthList.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react"
+import React, { useCallback, useEffect } from "react"
 import { Lessons as lessons } from "../../data.json"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native"
 import { setTitle } from "../../actions/TitleActions"
 import { Text } from "react-native-magnus"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useIsFocused } from "@react-navigation/native"
 
 type Props = NativeStackScreenProps<iLessonsStackParamList, "MonthList">
@@ -13,6 +13,7 @@ const MonthList = (props: Props): JSX.Element => {
 	//#region Hooks
 	const dispatch = useDispatch()
 	const isFocused = useIsFocused()
+	const progress = useSelector(state => state.lessons)
 	//#endregion
 
 	//#region Effects
@@ -24,6 +25,19 @@ const MonthList = (props: Props): JSX.Element => {
 	//#endregion
 
 	//#region Functions
+	const getBackgroundColor = useCallback(
+		(month: number) => {
+			if (progress[month - 1].every(day => day.completed)) {
+				return "#c6f6d5"
+			}
+			if (progress[month - 1].every(day => !day.completed)) {
+				return "#fed7d7"
+			}
+			return "#fefcbf"
+		},
+		[progress]
+	)
+
 	const handleMonth = (month: number) => {
 		props.navigation.push("DayList", { month })
 	}
@@ -37,7 +51,10 @@ const MonthList = (props: Props): JSX.Element => {
 					.map(month => (
 						<View key={month}>
 							<TouchableOpacity
-								style={styles.touchable}
+								style={[
+									styles.touchable,
+									{ backgroundColor: getBackgroundColor(month) }
+								]}
 								onPress={() => handleMonth(month)}>
 								<Text>Month {month}</Text>
 							</TouchableOpacity>
